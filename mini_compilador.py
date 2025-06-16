6ddc2ba99a6f0f76da996af198aabbb17d79b767
DubanPadilla73 <duban.alfz@gmail.com>
2025-06-16 19:27:21 -0400

    Añadida interfaz interactiva con paso a paso y conversión a prefija/postfija

--- mini_compilador.py
+++ mini_compilador.py
@@ -44,18 +44,15 @@ def procesar_linea(entrada):
         nombre_variable = tokens[0]
         expresion = tokens[2:]  # lo que viene después del igual
         expresion_con_valor = reemplazar_variables(expresion)
-        print("Evaluando:", ' '.join(expresion_con_valor))
         resultado = evaluar(expresion_con_valor)
         if resultado is not None:
             variables[nombre_variable] = resultado
-            print(f"Asignado: {nombre_variable} = {resultado}")
-            print("Variables actuales:", variables)
+        return expresion, resultado
     else:
         # Evaluación
         expresion_con_valor = reemplazar_variables(tokens)
         resultado = evaluar(expresion_con_valor)
-        print(f"Resultado: {resultado}")
-    return expresion
+    return tokens, resultado
 
 
 def papomudas(operador):
@@ -93,18 +90,96 @@ def infija_a_posfija(tokens):
                 salida.append(pila.pop())
             pila.pop()  # Elimina el paréntesis de apertura
         else:  # Es un operador
-            while (pila and pila[-1] != '(' and papomudas(pila[-1]) >= papomudas(token)):
+            while (pila and pila[-1] != "(" and papomudas(pila[-1]) >= papomudas(token)):
                 salida.append(pila.pop())
             pila.append(token)
     while pila:
         salida.append(pila.pop())
     return salida
 
+# Conversión de notación infija a prefija
 
-# Ejemplo de uso
-entrada = "x = (3 + 43) * 200 / (1 - 5) ^ 2 ^ 3"
-tokens = tokenizador(entrada)
-print("Tokens:", tokens)
-expresion = tokenizador(procesar_linea(entrada))
-posfija = infija_a_posfija(expresion)
-print("Postfija:", ' '.join(posfija))
+
+def infija_a_prefija(tokens):
+    tokens = tokens[::-1]  # Invertir la lista de tokens
+    for i in range(len(tokens)):
+        if tokens[i] == '(':
+            tokens[i] = ')'
+        elif tokens[i] == ')':
+            tokens[i] = '('
+    salida = infija_a_posfija(tokens)
+    return salida[::-1]  # Invertir la salida para obtener la notación prefija
+
+# Paso a paso
+
+
+def paso_a_paso(posfija):
+    pila = []
+
+    for token in posfija:
+        if re.match(r"\d+|\d+\.\d+", str(token)):  # Verifica si es un número
+            pila.append(int(token))
+        elif token in variables:
+            pila.append((variables[token]))
+        elif token in ["+", "-", "*", "/", "^"]:
+            if len(pila) < 2:
+                print("Error: No hay suficientes operandos en la pila.")
+                return
+            b = pila.pop()
+            a = pila.pop()
+            if token == "+":
+                resultado = a + b
+            elif token == "-":
+                resultado = a - b
+            elif token == "*":
+                resultado = a * b
+            elif token == "/":
+                if b == 0:
+                    print("División por cero.")
+                resultado = a / b
+            elif token == "^":
+                resultado = a ** b
+            print(f"{a} {token} {b} = {resultado}")
+            pila.append(resultado)
+        else:
+            print(f"Token desconocido: {token}")
+            return
+    if len(pila) == 1:
+        resultado_final = pila.pop()
+    else:
+        print("Error: La pila no tiene un único resultado al final.")
+        return
+
+
+# Interfaz de usuario
+activo = True
+
+while activo:
+    print("-"*5 + " Entrada " + "-"*5)
+    entrada = str(input("Entrada: "))
+
+    print("\n""-"*5 + " Salida " + "-"*5)
+    print("Evaluacion paso a paso:\n")
+
+    expresion, resultado = procesar_linea(entrada)
+    posfija = infija_a_posfija(expresion)
+    prefija = infija_a_prefija(expresion)
+
+    paso_a_paso(posfija)
+    print("\n")
+
+    token = tokenizador(entrada)
+    if variables in token:
+        nombre_variable = token[0]
+        print(f"Asignación: {nombre_variable} = {resultado}")
+        print(f"Variable: {variables}")
+
+    print("\nPrefija:", " ".join(prefija))
+    print("Postfija:", " ".join(posfija))
+    print("\n")
+
+    print("Desea continuar? (s/n): ")
+    continuar = str(input()).lower()
+    if continuar != 's':
+        activo = False
+        print("Saliendo del programa...")
